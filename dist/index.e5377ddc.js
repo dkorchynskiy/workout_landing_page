20771617d0c2fa97dafb90237101b08becca06eb
dmitriy korchynskiy <dmitriy.korchynskiy@gmail.com>
2022-06-15 20:15:20 +0300

    modified by scss

---
+++ dist/index.e5377ddc.js
@@ -0,0 +1,2 @@
+window.addEventListener("resize",(function(e){var n=document.querySelector("details.navigation");window.innerWidth>=1024?n.open=!0:n.open=!1}));
+//# sourceMappingURL=index.e5377ddc.js.map
